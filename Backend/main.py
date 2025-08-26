75a5cb8953fb75be12576ed751788b8b84eb5f38
Apurav <apurav0711@gmail.com>
2025-08-26 11:50:44 +0530

    renamed:    Backend/fast_api.py -> Backend/main.py

--- Backend/main.py
+++ Backend/main.py
@@ -555,6 +555,6 @@ if __name__ == "__main__":
         "aegis_complete_api:app",
         host="0.0.0.0",
         port=8000,
-        reload=True,
+        reload=False,
         log_level="info"
     )
