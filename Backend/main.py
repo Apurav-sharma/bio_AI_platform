307db20c77fa972b897d9d000f9f6ff40adecaf3
Apurav <apurav0711@gmail.com>
2025-08-26 11:52:12 +0530

    modified:   Backend/main.py

--- Backend/main.py
+++ Backend/main.py
@@ -554,7 +554,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "aegis_complete_api:app",
         host="0.0.0.0",
-        port=8000,
+        port=10000,
         reload=False,
         log_level="info"
     )
